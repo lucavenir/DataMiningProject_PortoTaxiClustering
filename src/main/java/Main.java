abc5f8928fea9fa5947e084af082e28cb21dd7c9
Met <matteo.stringher@studenti.unipd.it>
2017-04-29 16:53:58 +0200

    Head

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+public class Main {
+    public static void main(String[] args) {
+    	//SparkConf sparkConf = new SparkConf(true).setAppName("Compute primes");
+        //JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        System.out.println("Heello World");
+    }
+}
