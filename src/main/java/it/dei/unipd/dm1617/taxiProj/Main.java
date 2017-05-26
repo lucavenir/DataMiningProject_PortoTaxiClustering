32fd3c302ddd19c75d1d4cc9a4bd0c9279f27126
CR <cr.git.mail@gmail.com>
2017-05-26 20:25:21 +0200

    riorganizzato un po' il Main

--- src/main/java/it/dei/unipd/dm1617/taxiProj/Main.java
+++ src/main/java/it/dei/unipd/dm1617/taxiProj/Main.java
@@ -33,6 +33,14 @@ import java.nio.file.Path;
  */
 public class Main {
 	
+	
+	private static final int PAM = 0;
+	private static final int CLARA = 1;
+	private static final int CLARAFAST = 2;
+	private static final int CLARANS = 3;
+	private static final int KMEANS = 4;
+	
+	
     public static void main(String[] args) {
     	/*
     	 * Per configurare correttamente:
@@ -111,15 +119,20 @@ public class Main {
 	        Path dataPath = Paths.get(projectPath, "/data/trainFiltered");
 	        
 	        if (Files.exists(dataPath))
-	           	positions = InputOutput.readCleanDataset(ss, projectPath + "/data/trainFiltered");
-	        else {
+	        {
+	        	positions = InputOutput.readCleanDataset(ss, projectPath + "/data/trainFiltered");
+	        }
+	        else
+	        {
 	        	// Leggi il dataset
 	        	positions = InputOutput.readOriginalDataset(ss, projectPath + "/data/" + dataset);
 	        	
 	        	// Salva per future esecuzioni
 	        	InputOutput.write(positions, projectPath + "/data/trainFiltered");
 	        }
-        } else { // Non e' necessario per il sample che e' molto veloce da caricare e pulire
+        }
+        else
+        { // Non e' necessario per il sample che e' molto veloce da caricare e pulire
         	positions = InputOutput.readOriginalDataset(ss, projectPath + "data/" + dataset);
         }
         System.out.println("loaded");
@@ -145,6 +158,10 @@ public class Main {
         
         
         
+        
+        
+        
+        
         //System.out.println("Prova: " + positions.partitions().size());
         System.out.println("Numero righe prima clustering: " +  positions.count());
         /*
@@ -157,146 +174,147 @@ public class Main {
          * L'id viene eliminato
          */
        
-        JavaRDD<Vector> K_meansData = positions.map((p)->p.toVector()).cache();
+        JavaRDD<Vector> K_meansData = null;//viene inizializzato dopo, nello switch
         		  
         
-        /*
-         * Crea un clustering k means
-         * Da ricordare che K-means sfrutta la distanza euclidea L2 e non avrebbe senso usare un'altra metrica.
-           Quindi i punti vengono considerati come
-           planari e non considerano il fatto che la reale distanza dipenda anche dalla curva della terra.
-         * Nell'implementazione dell'algoritmo PAM, possiamo invece utilizzare la nostra distanza.
-         * Da wikipedia:
-           A medoid can be defined as the object of a cluster whose average dissimilarity to all the objects in the cluster
-           is minimal. (i.e. it is a most centrally located point in the cluster)
-         */
+        
         
         int numIterations = 60;
-        int k = 63;
-        
-        /*
-         * Inizializza il timer per misurare la performance di K Means
-         * System.currentTimeMillis() ritorna il # di ms da 1/1/1970
-         */
-        System.out.println("KMeans training...");
-        long init = System.currentTimeMillis();
-        // Ricorda: i centroidi non sono necessariamente punti del dataset
-        KMeansModel clusters = KMeans.train(K_meansData.rdd(), k, numIterations);
-        
-        // Misuro lo spazio occupato dal clustering (in totale e in kB)
-        long space = SizeEstimator.estimate(clusters)/1024
-        		+ SizeEstimator.estimate(K_meansData)/1024
-        		+ SizeEstimator.estimate(positions)/1024; // Sia chiaro: questo è solo un esempio di funzionamento
-        
-        // KMeans ha svolto il suo lavoro, Stop al cronometro!
-        long end = System.currentTimeMillis(); // Faremo la stessa cosa con KMedian
-        System.out.println("done.");
-
+        int k = 63;        
+        int alg = KMEANS;
         
         
-        
-        
-        
-        long t0_drawKMeans = System.nanoTime();
-        System.out.println("drawing positions...");
-        try
-        {
-        	new ClusteringDrawing(2000,2000).setAlfa(0.6).setLimits(41.35, -8.7, 41.05, -8.4).
-				draw(positions, clusters, null, false).
-				drawCenters(1, 1, 1, 1, clusters.clusterCenters(), 10).
-				save("data/images/kmeans.png");
-        }
-        catch (IOException e){
-            ss.close();
-            sc.close();
-            throw new RuntimeException("error saving img");
-        }
-        long t1_drawKMeans = System.nanoTime();
-        System.out.println("done ("+((t1_drawKMeans-t0_drawKMeans)/1000000)+"ms)");
-        
-        
-        
-    
-        /*
-         * Creo un'istanza di Timestamp da end-init: viene creata una data-ora (che sarà vicina a 00:00 del 1/1/1970);
-         * Serve per stampare minuti/secondi dell'esecuzione del clustering
-         */
-        Timestamp t = new Timestamp (end-init);
-        
-        // Calcolo dei punti a massima distanza dal centro del loro cluster + Print dei centri
-        Tuple2<Position[],Double[]> maxdist = Utils.calcolaMaxDistanze(clusters, positions);
-        
-        // Stampa dei risultati sopra ottenuti
-        for (int i=0; i<k; i++) {
-        	System.out.println("Cluster #" + (i+1) + ", con centro: " + clusters.clusterCenters()[i]);
-        	System.out.println("Punto a massima distanza: " + maxdist._1()[i] + " (d: " + maxdist._2()[i] + ")");
-        }
-        
-        /*
-         * Per qualche strano motivo (che non voglio indagare),
-         * Timestamp ha eliminato i metodi .getMinute() e . getSecond().
-         * Allora tocca "passare" per la classe LocalDateTime che questi metodi li ha. 
-         */
-        System.out.print("K-means time: ");
-        System.out.println(t.toLocalDateTime().getMinute() + " minutes and " + t.toLocalDateTime().getSecond() + " seconds");
-        System.out.println("k=" + k);
-        System.out.println("K-means space: " + space + " kB");
-        // E' solo un esempio. Non sara' la distanza che noi dobbiamo minimizzare.
-        double WSSSE = clusters.computeCost(K_meansData.rdd());
-        System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
-        
-        
-        
-        
-        
-        
-        int kmedianAlg = 2;
         
         String imagePath = null;
         Position[] centers = null;
         Kmedian a = new Kmedian(positions);
-        int l = 4;	        
-        switch(kmedianAlg)
+        KMeansModel kmeansClusters= null;
+        int l = 4;	     
+        long t0 = System.nanoTime();
+        switch(alg)
         {
-        case 0:
-	        centers = a.getPAMCenters(63, l);
+        case PAM:
+        	System.out.println("running PAM...");
+	        centers = a.getPAMCenters(k, l);
 	        System.out.println(a.objectiveFunction(centers));
 	        imagePath="data/images/kmedianPAM.png";
         	break;
-        case 1:
-	        centers = a.getCLARACenters(63, l);
+        case CLARA:
+        	System.out.println("running CLARA...");
+	        centers = a.getCLARACenters(k, l);
 	        System.out.println(a.objectiveFunction(centers));
 	        imagePath="data/images/kmedianCLARA.png";
         	break;
-        case 2:
-	        centers = a.getCLARAFASTCenters(63);
+        case CLARAFAST:
+        	System.out.println("running CLARAFAST...");
+	        centers = a.getCLARAFASTCenters(k);
 	        System.out.println(a.objectiveFunction(centers));
 	        imagePath="data/images/kmedianCLARAFAST.png";
         	break;
-        case 3:
-	        centers = a.getCLARANSCenters(63);
+        case CLARANS:
+        	System.out.println("running CLARANS...");
+	        centers = a.getCLARANSCenters(k);
 	        System.out.println(a.objectiveFunction(centers));
 	        imagePath="data/images/kmedianCLARANS.png";
         	break;
+        case KMEANS:
+        	/*
+             * Crea un clustering k means
+             * Da ricordare che K-means sfrutta la distanza euclidea L2 e non avrebbe senso usare un'altra metrica.
+               Quindi i punti vengono considerati come
+               planari e non considerano il fatto che la reale distanza dipenda anche dalla curva della terra.
+             * Nell'implementazione dell'algoritmo PAM, possiamo invece utilizzare la nostra distanza.
+             * Da wikipedia:
+               A medoid can be defined as the object of a cluster whose average dissimilarity to all the objects in the cluster
+               is minimal. (i.e. it is a most centrally located point in the cluster)
+             */
+        	System.out.println("running KMEANS...");
+        	K_meansData = positions.map((p)->p.toVector()).cache();
+            kmeansClusters = KMeans.train(K_meansData.rdd(), k, numIterations);
+	        imagePath="data/images/kmeans.png";
+            break;
         }
+        long t1 = System.nanoTime();
+        System.out.println("clustering completed ("+(t1-t0)+"ms)");
+
         
         
-        long drawPAM0 = System.nanoTime();
+        //DISEGNA
+        
+        
+        long draw_t0 = System.nanoTime();
         System.out.println("drawing positions...");
         try
         {
-        	new ClusteringDrawing(2000,2000).setAlfa(0.6).setLimits(41.35, -8.7, 41.05, -8.4).
-				draw(a.partitionAsRDD(centers),centers.length,null,false).
-				drawCenters(1, 1, 1, 1, centers, 10).
-				save(imagePath);
+        	if(alg==KMEANS)
+        	{
+        		new ClusteringDrawing(2000,2000).setAlfa(0.6).setLimits(41.35, -8.7, 41.05, -8.4).
+					draw(positions, kmeansClusters, null, false).
+					drawCenters(1, 1, 1, 1, kmeansClusters.clusterCenters(), 10).
+					save(imagePath);
+        	}
+        	else
+        	{
+	        	new ClusteringDrawing(2000,2000).setAlfa(0.6).setLimits(41.35, -8.7, 41.05, -8.4).
+					draw(a.partitionAsRDD(centers),centers.length,null,false).
+					drawCenters(1, 1, 1, 1, centers, 10).
+					save(imagePath);
+        	}
         }
         catch (IOException e)  {
             ss.close();
             sc.close();
             throw new RuntimeException("error saving img");
         }
-        long drawPAM1 = System.nanoTime();
-        System.out.println("done ("+((drawPAM1-drawPAM0)/1000000)+"ms)");
+        long draw_t1 = System.nanoTime();
+        System.out.println("done ("+((draw_t1-draw_t0)/1000000)+"ms)");
+        
+        
+        
+        
+        
+        if(alg==KMEANS)
+        {
+	        
+	        // Misuro lo spazio occupato dal clustering (in totale e in kB)
+	        long space = SizeEstimator.estimate(kmeansClusters)/1024
+	        		+ SizeEstimator.estimate(K_meansData)/1024
+	        		+ SizeEstimator.estimate(positions)/1024; // Sia chiaro: questo è solo un esempio di funzionamento
+	    
+	        /*
+	         * Creo un'istanza di Timestamp da end-init: viene creata una data-ora (che sarà vicina a 00:00 del 1/1/1970);
+	         * Serve per stampare minuti/secondi dell'esecuzione del clustering
+	         */
+	        Timestamp t = new Timestamp (t1-t0);
+	        
+	        // Calcolo dei punti a massima distanza dal centro del loro cluster + Print dei centri
+	        Tuple2<Position[],Double[]> maxdist = Utils.calcolaMaxDistanze(kmeansClusters, positions);
+	        
+	        // Stampa dei risultati sopra ottenuti
+	        for (int i=0; i<k; i++) {
+	        	System.out.println("Cluster #" + (i+1) + ", con centro: " + kmeansClusters.clusterCenters()[i]);
+	        	System.out.println("Punto a massima distanza: " + maxdist._1()[i] + " (d: " + maxdist._2()[i] + ")");
+	        }
+	        
+	        /*
+	         * Per qualche strano motivo (che non voglio indagare),
+	         * Timestamp ha eliminato i metodi .getMinute() e . getSecond().
+	         * Allora tocca "passare" per la classe LocalDateTime che questi metodi li ha. 
+	         */
+	        System.out.print("K-means time: ");
+	        System.out.println(t.toLocalDateTime().getMinute() + " minutes and " + t.toLocalDateTime().getSecond() + " seconds");
+	        System.out.println("k=" + k);
+	        System.out.println("K-means space: " + space + " kB");
+	        // E' solo un esempio. Non sara' la distanza che noi dobbiamo minimizzare.
+	        double WSSSE = kmeansClusters.computeCost(K_meansData.rdd());
+	        System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
+	        
+	        
+        }
+        
+        
+        
+        
         
         // Chiudi Spark
         ss.close();
